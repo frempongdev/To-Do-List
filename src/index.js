bb0ff10e9124520795678ae46316fb3670c43c19
Frempong <samankwaa.frempong@gmail.com>
2023-01-25 12:06:05 +0000

    Setup Webpack

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import './style.css';
+import Logo from './assets/event1.png'
+
+function component() {
+  //   const element = document.createElement('div');
+  
+  //   // Lodash, now imported by this script
+  //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //   element.classList.add('hello');
+
+  //     // Add the image to our existing div.
+  // const myLogo = new Image();
+  // myLogo.src = Logo;
+
+  // element.appendChild(myLogo);
+  
+  //   return element;
+
+  
+  }
+  
+  document.body.appendChild(component());
+
+ 
